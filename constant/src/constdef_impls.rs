59050b7f5713ece333a70bac8bdb85bc0468dc3e
Sayan Nandan <nandansayan@outlook.com>
2021-09-30 22:06:18 -0700

    Add all impls for `core` that can be const
    Signed-off-by: Sayan Nandan <nandansayan@outlook.com>

--- constant/src/constdef_impls.rs
+++ constant/src/constdef_impls.rs
@@ -1,29 +1,13 @@
+mod alloc;
+mod core;
 mod primitive;
 
-mod alloc_crate {
-    use crate::Constdef;
-
-    impl<T> Constdef for Vec<T> {
-        const DEFAULT: Self = new!();
-    }
-
-    impl Constdef for String {
-        const DEFAULT: Self = new!();
-    }
-}
-
-mod std_collections {
-    use crate::Constdef;
-    use std::collections::LinkedList;
-    impl<T> Constdef for LinkedList<T> {
-        const DEFAULT: Self = LinkedList::new();
-    }
-}
-
-mod core_mem {
-    use crate::Constdef;
-    use core::mem::MaybeUninit;
-    impl<T> Constdef for MaybeUninit<T> {
-        const DEFAULT: Self = Self::uninit();
+mod std {
+    mod collections {
+        use crate::Constdef;
+        use std::collections::LinkedList;
+        impl<T> Constdef for LinkedList<T> {
+            const DEFAULT: Self = LinkedList::new();
+        }
     }
 }
